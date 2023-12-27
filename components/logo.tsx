c915afc3dbad0b22993904e7dfa544c40ec326c1
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-12-27 12:26:57 +0530

    added new logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -7,9 +7,15 @@ export const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 flex">
-        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          height={30}
+          width={30}
+          className="bg-white"
+        />
         <p className={cn("text-lg text-primary pb-1 font-switzerBold")}>
-          Collab
+          Chatty
         </p>
       </div>
     </Link>
