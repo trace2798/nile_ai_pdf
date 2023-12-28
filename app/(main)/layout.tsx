e86a8ada96036e3f92eff26f639f10204e89e3a9
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-12-28 12:13:51 +0530

    added nile.userId on layout too

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 import { Toaster } from "sonner";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  configureNile(cookies().get("authData"), null);
+  configureNile(cookies().get("authData"), nile.userId);
   console.log("showing tenants page for user: " + nile.userId);
   if (!nile.userId) {
     redirect("/login");
